73399d527f81ca5fd98f0cef60a71002d507232e
gonzalo <gonzalo@agencycoda.com>
2022-06-29 15:16:21 -0300

    fix: delete ApiResponse method

---
+++ src/models/config_test.js
@@ -0,0 +1,15 @@
+/*const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ConfigTest extends Model {
+    static associate(modeld) {
+
+    }
+  }
+
+  ConfigTest.init({
+
+  })
+
+  return ConfigTest;
+}*/
